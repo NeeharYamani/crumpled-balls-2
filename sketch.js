33e162caabce5bf435a5f1dd55c954647a508221
NeeharYamani <74959911+NeeharYamani@users.noreply.github.com>
2021-01-26 18:26:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustbin1, dustbin2, dustbin3, dustbinImage;
+var ground;
+var paper;
+
+function preload() {
+	dustbinImage = loadImage("images/dustbingreen.png")
+}
+
+function setup() {
+	
+	createCanvas(1400,600);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper (100,100,70);
+	ground = new Ground (700,600,1400,10);
+	dustbin1 = new Dustbin (1020,520,20,140);
+	dustbin2 = new Dustbin (1090,590,155,20)
+	dustbin3 = new Dustbin (1170,520,20,140);
+
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  
+  background("gray");
+
+  ground.display();
+  
+  dustbin1.display();
+  dustbin3.display();
+  dustbin2.display();
+  
+  imageMode(CENTER);
+  image(dustbinImage,1090,525,195,160);
+
+  paper.display();
+ 
+}
+
+function keyPressed(){
+
+	if (keyCode === RIGHT_ARROW){
+
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:160,y:-115})
+	}
+}
